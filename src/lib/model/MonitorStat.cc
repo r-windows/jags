be22746e952985b4f1ed474ee29aa29bdd75b920
martyn_plummer <martyn_plummer@sourceforge.net>
2024-12-04 08:06:10 +0000

    Adding missing source file for MonitorStat

---
+++ src/lib/model/MonitorStat.cc
@@ -0,0 +1,14 @@
+#include <config.h>
+
+#include <model/MonitorStat.h>
+
+using std::vector;
+
+namespace jags {
+
+    vector<double> MonitorStat::weight(unsigned int chain) const
+    {
+	return vector<double>(length(), 1.0);
+    }
+
+}
